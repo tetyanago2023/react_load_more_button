c8bc8574f62a5ae645a3501a7fada86a89646b37
go2023 <tetyanago2018@gmail.com>
2024-02-08 20:47:05 -0800

    code was refactored

--- src/components/load-more-button/index.jsx
+++ src/components/load-more-button/index.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
+
+const API_URL = "https://dummyjson.com/products";
+const PRODUCTS_LIMIT = 20;
 
 export default function LoadMoreData() {
     const [loading, setLoading] = useState(false);
@@ -8,15 +10,10 @@ export default function LoadMoreData() {
     const [count, setCount] = useState(0);
     const [disableButton, setDisableButton] = useState(false);
 
-    async function fetchProducts() {
+    const fetchProducts = async () => {
         try {
             setLoading(true);
-            const response = await fetch(
-                `https://dummyjson.com/products?limit=20&skip=${
-                    count === 0 ? 0 : count * 20
-                }`
-            );
-
+            const response = await fetch(`${API_URL}?limit=${PRODUCTS_LIMIT}&skip=${count * PRODUCTS_LIMIT}`);
             const result = await response.json();
 
             if (result && result.products && result.products.length) {
@@ -25,41 +22,42 @@ export default function LoadMoreData() {
             }
 
             console.log(result);
-        } catch (e) {
-            console.log(e);
+        } catch (error) {
+            console.error(error);
             setLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
         fetchProducts();
     }, [count]);
 
     useEffect(() => {
-        if (products && products.length === 100) setDisableButton(true);
+        if (products.length === 100) setDisableButton(true);
     }, [products]);
 
     if (loading) {
-        return <div>Loading data ! Please wait.</div>;
+        return <div>Loading data! Please wait.</div>;
     }
 
     return (
         <div className="load-more-container">
             <div className="product-container">
-                {products && products.length
-                    ? products.map((item) => (
-                        <div className="product" key={item.id}>
+                {products.length ? (
+                    products.map((item, index) => (
+                        // <div className="product" key={item.id || index}> this key implementation gives following warning in the browser console : Warning: Each child in a list should have a unique "key" prop.
+                        <div className="product" key={index}>
                             <img src={item.thumbnail} alt={item.title} />
                             <p>{item.title}</p>
                         </div>
                     ))
-                    : null}
+                ) : null}
             </div>
             <div className="button-container">
-                <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+                <button disabled={disableButton} onClick={() => setCount((prevCount) => prevCount + 1)}>
                     Load More Products
                 </button>
-                {disableButton ? <p>You have reached to 100 products</p> : null}
+                {disableButton && <p>You have reached 100 products</p>}
             </div>
         </div>
     );
